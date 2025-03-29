7e0f18c48de20b55a3c29887322bc06954eef03b
BiMalxMe <journeyxbimal@gmail.com>
2025-03-29 17:26:24 +0545

    AddingRespectiveViewsAndUrls

--- emp_app/urls.py
+++ emp_app/urls.py
@@ -20,8 +20,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('', views.all_emp, name="all_emp"),
-    path('', views.add_emp, name="add_emp"),
-    path('', views.remove_emp, name="remove_emp"),
-    path('', views.filter_emp, name="filter_emp")
+    path('view_all_emp', views.view_all_emp, name="view_all_emp"),#the string youwrite in the name canbe used in href as {% url 'view_all_emp' %}
+    path('add_emp', views.add_emp, name="add_emp"),
+    path('remove_emp', views.remove_emp, name="remove_emp"),
+    path('filter_emp', views.filter_emp, name="filter_emp")
 ]
