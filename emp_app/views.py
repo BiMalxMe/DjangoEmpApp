e40233ff65989ccc9c16bc760761d8ddc65cc856
BiMalxMe <journeyxbimal@gmail.com>
2025-03-29 18:42:47 +0545

    AddingFunctionRelatedtoAddingtheEmployee

--- emp_app/views.py
+++ emp_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from .models import Employee,Role,Department
+from datetime import datetime
 
 # Create your views here.
 
@@ -13,9 +14,31 @@ def view_all_emp(request):
     }
     print(context)
     return render(request,"view_all_emp.html",context)
-
 def add_emp(request):
-    return render(request,"add_emp.html")
+    if request.method == "POST":
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        dept = request.POST['dept']
+        salary = int(request.POST['salary'])
+        bonus = int(request.POST['bonus'])
+        role = request.POST['role']
+        phone = int(request.POST['phone'])
+        new_emp = Employee(
+            first_name=first_name,
+            last_name=last_name,
+            dept_id=dept,
+            salary=salary,
+            bonus=bonus,
+            role_id=role,
+            phone=phone,
+            hire_date= datetime.now() # Save hire_date
+        )
+        new_emp.save() 
+        return HttpResponse("Employee Added")
+    elif request.method == "GET":    
+        return render(request,"add_emp.html")
+    else:
+        return HttpResponse("An Error Occurred")
 
 def remove_emp(request):
     return render(request,"remove_emp.html")
