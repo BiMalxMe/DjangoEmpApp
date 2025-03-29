0d47b33b9166efc7a9355feede7959491c71ee49
BiMalxMe <journeyxbimal@gmail.com>
2025-03-29 21:57:11 +0545

    #7_DoneAfterDefiningModelForTheAcessOfDatabaseInTheAdminDashboard

--- emp_app/admin.py
+++ emp_app/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import Employee, Role ,Department
 
 # Register your models here.
+
+
+#Yesle admin le acess anee modify garna milne models lai list garxa
 admin.site.register(Employee)
 admin.site.register(Department)
 admin.site.register(Role)
